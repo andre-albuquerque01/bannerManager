7c07e3c74e173e531504cded2d8d888530e6e254
andre <andrealbuquerque2001@gmail.com>
2024-08-22 18:13:09 -0300

    feat: create controller

---
+++ Api/app/Http/Controllers/Api/BannerController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BannerRequest;
+use App\Service\BannerService;
+use Illuminate\Http\Request;
+
+class BannerController extends Controller
+{
+    private $bannerService;
+
+    public function __construct(BannerService $bannerService)
+    {
+        $this->bannerService = $bannerService;
+    }
+
+    public function index()
+    {
+        return $this->bannerService->index();
+    }
+    public function show(string $id)
+    {
+        return $this->bannerService->show($id);
+    }
+    public function store(BannerRequest $request)
+    {
+        return $this->bannerService->store($request->validated());
+    }
+    public function update(BannerRequest $request, string $id)
+    {
+        return $this->bannerService->update($request->validated(), $id);
+    }
+    public function destroy(string $id)
+    {
+        return $this->bannerService->destroy($id);
+    }
+}
