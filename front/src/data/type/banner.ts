ef994e5258b2b399181776aa2f2aea3e81d0ab57
andre <andrealbuquerque2001@gmail.com>
2024-08-23 14:57:27 -0300

    feat: next application

---
+++ front/src/data/type/banner.ts
@@ -0,0 +1,12 @@
+export interface BannerInterface {
+  idBanner: string
+  nameMidia: string
+  veiculo: string
+  dimensao: string
+  tamanho: string
+  looping: string
+  tempo: string
+  complexidade: string
+  tipo: string
+  statusBanner: string
+}
