06da60d15b4480ac3680fabab7600fcb3e28134e
andre <andrealbuquerque2001@gmail.com>
2024-09-21 14:00:05 -0300

    feat: new database

---
+++ Api/database/migrations/2024_09_20_195418_create_business_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('business', function (Blueprint $table) {
+            $table->ulid('idBusiness')->primary();
+            $table->string('campanha');
+            $table->string('cliente');
+            $table->string('agencia');
+            $table->boolean('destaque');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('business');
+    }
+};
