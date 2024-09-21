08862307e67b79325c11d513d8145046284d4d1e
andre <andrealbuquerque2001@gmail.com>
2024-09-21 14:16:51 -0300

    feat: update service

--- Api/app/Service/BannerService.php
+++ Api/app/Service/BannerService.php
@@ -6,8 +6,6 @@ use App\Exceptions\GeneralExceptionCatch;
 use App\Http\Resources\BannerResource;
 use App\Http\Resources\GeneralResource;
 use App\Models\Banner;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 
 class BannerService
 {
@@ -34,23 +32,6 @@ class BannerService
     public function store(array $data)
     {
         try {
-            if (isset($data['nameMidia'])) {
-                $media = $data['nameMidia'];
-                $extension = $media->getClientOriginalExtension();
-                $originalName = pathinfo($media->getClientOriginalName(), PATHINFO_FILENAME);
-
-                if ($extension != null) {
-                    $sizeInBytes = $media->getSize();
-                    $data['tamanho'] = $this->formatSizeUnits($sizeInBytes);
-                    $data['complexidade'] = strtoupper($extension);
-                    $filename = $originalName . '.' . $extension;
-                    Storage::disk('public')->put('img/banner/' . $filename, file_get_contents($media));
-                    $data['nameMidia'] = $filename;
-                }
-            } else {
-                $data['nameMidia'] = null;
-            }
-
             Banner::create($data);
             return new GeneralResource(['message' => 'success']);
         } catch (\Exception $e) {
@@ -64,28 +45,8 @@ class BannerService
             $banner = Banner::where('idBanner', $id)->first();
 
             if (!$banner) {
-                return new GeneralResource(["message" => "Unathorized"]);
+                return new GeneralResource(["message" => "not found"]);
             }
-
-            if (isset($data['nameMidia'])) {
-                $media = $data['nameMidia'];
-                $extension = $media->getClientOriginalExtension();
-                $originalName = pathinfo($media->getClientOriginalName(), PATHINFO_FILENAME);
-
-                if ($extension != null) {
-                    $sizeInBytes = $media->getSize();
-                    $data['tamanho'] = $this->formatSizeUnits($sizeInBytes);
-                    $data['complexidade'] = strtoupper($extension);
-                    $filename = $originalName . '.' . $extension;
-                    Storage::disk('public')->put('img/banner/' . $filename, file_get_contents($media));
-                    $data['nameMidia'] = $filename;
-                } else {
-                    $data['nameMidia'] = $banner->nameMidia;
-                }
-            } else {
-                $data['nameMidia'] = $banner->nameMidia;
-            }
-
             $banner->update($data);
             return new GeneralResource(['message' => 'success']);
         } catch (\Exception $e) {
@@ -112,29 +73,9 @@ class BannerService
     {
         try {
             $banner = Banner::where('idBanner', $id)->first();
-            $path = storage_path('app/public/img/banner/' . $banner->nameMidia);
-            return response()->download($path,  $banner->nameMidia);
+            return response()->download($banner->urlMidia,  $banner->title);
         } catch (\Exception $e) {
-            throw new GeneralExceptionCatch('Error, downLoadImage');
+            throw new GeneralExceptionCatch('Error, downloadImage');
         }
-    }
-
-    private function formatSizeUnits($bytes)
-    {
-        if ($bytes >= 1073741824) {
-            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
-        } elseif ($bytes >= 1048576) {
-            $bytes = number_format($bytes / 1048576, 2) . ' MB';
-        } elseif ($bytes >= 1024) {
-            $bytes = number_format($bytes / 1024, 2) . ' KB';
-        } elseif ($bytes > 1) {
-            $bytes = $bytes . ' bytes';
-        } elseif ($bytes == 1) {
-            $bytes = $bytes . ' byte';
-        } else {
-            $bytes = '0 bytes';
-        }
-
-        return $bytes;
     }
 }
