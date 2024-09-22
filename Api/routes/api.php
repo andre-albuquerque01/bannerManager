280e115f80eb726353e8a40d73f633e946c3ce61
andre <andrealbuquerque2001@gmail.com>
2024-09-22 11:13:00 -0300

    feat: update api route

--- Api/routes/api.php
+++ Api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\BannerController;
+use App\Http\Controllers\Api\BusinessController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,17 +11,20 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
-    Route::post('user', [UserController::class, 'store']);
+    Route::post('userStore', [UserController::class, 'store']);
     Route::post('login', [UserController::class, 'login']);
     Route::post('sendTokenRecover', [UserController::class, 'sendTokenRecover']);
     Route::put('resetPassword', [UserController::class, 'resetPassword']);
     
     Route::get('bannerIndex', [BannerController::class, 'index']);
-    Route::get('downLoadImage/{id}', [BannerController::class, 'downLoadImage']);
+    Route::get('downloadImage/{id}', [BannerController::class, 'downloadImage']);
+    Route::get('businessIndex', [BusinessController::class, 'index']);
     
     Route::middleware('auth:sanctum')->group(function () {
-        Route::post('banner/update/{id}', [BannerController::class, 'update']);
+        Route::get('businessAll', [BusinessController::class, 'indexAll']);
+        Route::apiResource('business', BusinessController::class);
         Route::apiResource('banner', BannerController::class);
+        Route::put('banner/update/{id}', [BannerController::class, 'update']);
         Route::apiResource('user', UserController::class);
         Route::post('logout', [UserController::class, 'logout']);
     });
