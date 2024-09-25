719fedb67d92e1fd67955b91d762ce82357874be
andre <andrealbuquerque2001@gmail.com>
2024-09-25 10:29:55 -0300

    feat: update page

--- front/src/app/business/page.tsx
+++ front/src/app/business/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
         <h1 className="text-2xl font-semibold text-zinc-800">Campanhas</h1>
         <Link
           href="/business/insert"
-          className="px-6 py-2 rounded-lg bg-zinc-500 text-white transition duration-300 hover:bg-zinc-900"
+          className="px-6 py-2 rounded-lg bg-blue-600 border-none text-white transition duration-300 hover:bg-blue-800"
         >
           Inserir campanha
         </Link>
