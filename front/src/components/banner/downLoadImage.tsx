a32a16b06676abc841133d3bd17d5bed0c875547
andre <andrealbuquerque2001@gmail.com>
2024-09-25 10:24:52 -0300

    feat: update component

--- front/src/components/banner/downLoadImage.tsx
+++ front/src/components/banner/downLoadImage.tsx
@@ -1,44 +1,23 @@
 'use client'
-import ApiRoute from '@/data/apiRoute'
 import { Download } from 'lucide-react'
 
-export const DownLoadImageComponent = ({
-  id,
-  fileExtension,
-  fileName,
-}: {
-  id: string
-  fileExtension: string
-  fileName: string[]
-}) => {
-  const handleDownload = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-  ) => {
-    e.preventDefault()
-
-    const response = await ApiRoute(`/downLoadImage/${id}`)
-    if (!response.ok) {
-      console.error('Erro ao baixar a imagem')
-      return
-    }
-    const blob = await response.blob()
-    const url = window.URL.createObjectURL(blob)
-
+export const DownloadImageComponent = ({ urlMidia }: { urlMidia: string }) => {
+  const handleDownload = (url: string) => {
     const link = document.createElement('a')
     link.href = url
-    link.setAttribute('download', `${fileName}.${fileExtension.toLowerCase()}`)
+    link.setAttribute('download', 'media-file')
     document.body.appendChild(link)
     link.click()
     document.body.removeChild(link)
   }
 
   return (
-    <button
-      onClick={handleDownload}
+    <a
+      onClick={() => handleDownload(urlMidia)}
       className="flex items-center gap-2 px-4 py-2 bg-blue-500 transition duration-500 hover:bg-blue-300 cursor-pointer rounded"
       title="Baixar mídia"
     >
       <Download size={20} /> Download
-    </button>
+    </a>
   )
 }
